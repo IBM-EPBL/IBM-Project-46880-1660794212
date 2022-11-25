32b0025ac76c392cef8c9b211002ef944de20012
Rohan <89628684+Bigd4mher0@users.noreply.github.com>
2022-11-25 13:01:49 +0100

    Update app.py

--- FINAL DELIVERABLES/FINAL CODE/app.py
+++ FINAL DELIVERABLES/FINAL CODE/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from markupsafe import escape
 
 import ibm_db
-conn = ibm_db.connect("DATABASE=InventoryManagementSystem;HOSTNAME=ea286ace-86c7-4d5b-8580-3fbfa46b1c66.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31505;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xcw16231;PWD=bCbLMVLA2En8uHMN",'','')
+conn = ibm_db.connect("DATABASE=InventoryManagementSystem;HOSTNAME=824dfd4d-99de-440d-9991-629c01b3832d.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30119;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=yjp31702;PWD=EGg6eYAGQNUeO2dY",'','')
 
 import smtplib, ssl
 ## email.mime subclasses
